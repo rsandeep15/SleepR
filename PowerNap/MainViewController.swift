1e4ab6e33cd74436a4d7ec3f1bc4750a31867821
Sandeep Raghunandhan <saraghun@ucsd.edu>
2017-03-24 23:46:48 -0700

    Setup logout

---
+++ PowerNap/MainViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MainViewController.swift
+//  PowerNap
+//
+//  Created by Sandeep Raghunandhan on 3/24/17.
+//  Copyright © 2017 Sandeep Raghunandhan. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+
+class MainViewController: UIViewController {
+    
+    
+    let auth: FIRAuth = FIRAuth.auth()!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func logout(_ sender: Any) {
+        do {
+           try auth.signOut()
+            self.performSegue(withIdentifier: "logoutSegue", sender: nil)
+        }
+        catch FIRAuthErrorCode.errorCodeKeychainError {
+            print("Key chain error")
+        }
+        catch let error {
+            print(error.localizedDescription)
+        }
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
